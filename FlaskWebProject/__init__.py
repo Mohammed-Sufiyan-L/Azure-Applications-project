06336a06239efdfff2b46b105db723bd6a3edd7b
Mohammed-Sufiyan-L <loharsufi4656@gmail.com>
2026-04-28 14:17:04 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,21 +1,19 @@
-"""
-The flask application package.
-"""
 import logging
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
 Session(app)
 db = SQLAlchemy(app)
 with app.app_context():
     db.create_all()
 login = LoginManager(app)
 login.login_view = 'login'
-
 import FlaskWebProject.views

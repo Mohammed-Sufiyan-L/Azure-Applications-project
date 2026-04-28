75f6b52b9ba503496ac734621afa87290d1f906a
Mohammed-Sufiyan-L <loharsufi4656@gmail.com>
2026-04-28 12:23:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,9 +15,12 @@ class Config(object):
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = (
-    'mssql+pyodbc://' + SQL_USER_NAME + ':' + SQL_PASSWORD + 
-    '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + 
-    '?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes&Connection+Timeout=30'
+    'mssql+pyodbc://' + SQL_USER_NAME + ':' + SQL_PASSWORD +
+    '@' + SQL_SERVER + ',1433/' + SQL_DATABASE +
+    '?driver=ODBC+Driver+18+for+SQL+Server'
+    '&Encrypt=yes'
+    '&TrustServerCertificate=no'
+    '&Connection+Timeout=30'
 )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
